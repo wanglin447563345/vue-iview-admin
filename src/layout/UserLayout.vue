0021880ac1a98ee7e7ff3d9de5dd8693fa3f98f4
walee <447563345@qq.com>
2019-05-31 08:52:18 +0800

    按需加载iview

--- src/layout/UserLayout.vue
+++ src/layout/UserLayout.vue
@@ -4,8 +4,20 @@
         <router-view></router-view>
     </div>
 </template>
+
 <style lang="scss">
     .user_layout{
-        height: 1vh;
+        min-height: 100vh;
+        background: #f7f7f7;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    h1{
+        color: #2d8cf0;
+        font-size: 30px;
+        text-align: center;
+        margin-bottom: 60px;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import router from './router.js'
 import store from './store'
 import 'iview/dist/styles/iview.css';
 
-import { Button, Table } from 'iview';
+import { 
+  Button, 
+  Table,
+  Form,
+  Input,
+  FormItem
+
+} from 'iview';
 Vue.component('Button', Button);
 Vue.component('Table', Table);
+Vue.component('Form', Form);
+Vue.component('Input', Input);
+Vue.component('FormItem', FormItem);
 
 Vue.config.productionTip = false
 
