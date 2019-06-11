5c5c04c39d1470d68ab9507fb6ce594988c2b006
walee <447563345@qq.com>
2019-06-11 09:04:46 +0800

    路由

--- src/layout/LeftMenu.vue
+++ src/layout/LeftMenu.vue
@@ -1,61 +1,47 @@
 <template>
-  <Menu active-name="1-2" :open-names="['1']" :theme='theme'>
-        <Submenu name="1">
-          <template slot="title">
-            <Icon type="ios-analytics"/>Navigation One
-          </template>
-          <MenuGroup title="Item 1">
-            <MenuItem name="1-1">Option 1</MenuItem>
-            <MenuItem name="1-2">Option 2</MenuItem>
-          </MenuGroup>
-          <MenuGroup title="Item 2">
-            <MenuItem name="1-3">Option 3</MenuItem>
-            <MenuItem name="1-4">Option 4</MenuItem>
-          </MenuGroup>
+  <Menu active-name="dashboard" :theme="theme">
+    <Submenu name="1">
+            <template slot="title">
+                <Icon type="ios-analytics" />
+                主页
+            </template>
+            <MenuItem name="1-1" to='/home/analysis'>分析页</MenuItem>
         </Submenu>
-        <Submenu name="2">ß
-          <template slot="title">
-            <Icon type="ios-filing"/>Navigation Two
-          </template>
-          <MenuItem name="2-1">Option 5</MenuItem>
-          <MenuItem name="2-2">Option 6</MenuItem>
-          <Submenu name="3">
-            <template slot="title">Submenu</template>
-            <MenuItem name="3-1">Option 7</MenuItem>
-            <MenuItem name="3-2">Option 8</MenuItem>
-          </Submenu>
+        <Submenu name="2">
+            <template slot="title">
+                <Icon type="ios-filing" />
+                表格
+            </template>
+            <MenuItem name="2-1"  to='/table/basic_table'>基础表格</MenuItem>
+            <MenuItem name="2-2" to="/table/drag_table">可拖动表格</MenuItem>
         </Submenu>
         <Submenu name="4">
-          <template slot="title">
-            <Icon type="ios-cog"/>Navigation Three
-          </template>
-          <MenuItem name="4-1">Option 9</MenuItem>
-          <MenuItem name="4-2">Option 10</MenuItem>
-          <MenuItem name="4-3">Option 11</MenuItem>
-          <MenuItem name="4-4">Option 12</MenuItem>
+            <template slot="title">
+                <Icon type="ios-cog" />
+                表单
+            </template>
+            <MenuItem name="4-1" to='/form/basic_form'>基础表单</MenuItem>
         </Submenu>
-      </Menu>
+  </Menu>
 </template>
 
 <script>
-import {
-    Menu,
-    Submenu,
-    MenuGroup,
-    MenuItem
-} from "iview";
+import { Menu, Submenu, MenuItem } from "iview";
 export default {
-  props:{
+  props: {
     theme: {
       type: String,
-      default: 'dark'
+      default: "dark"
     }
   },
-    components: {
-        Menu,
-        Submenu,
-        MenuGroup,
-        MenuItem
-    }
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {
+    Menu,
+    Submenu,
+    MenuItem
+  }
 };
 </script>
--- src/router.js
+++ src/router.js
@@ -2,18 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import UserLayout from './layout/UserLayout.vue'
 import AdminLayout from './layout/AdminLayout.vue'
+import NProgress from 'nprogress'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/user',
       name: 'user',
+      hideInMenu: true,
       component: UserLayout,
       children: [
+        { 
+          path: "/user",
+          redirect: "/user/login"
+        },
         {
           path: 'login',
           name: 'login',
@@ -27,21 +33,80 @@ export default new Router({
       ]
     },
     {
-      path: '/admin',
-      name: 'admin',
-      redirect: '/admin/home',
+      path: '/',
       component: AdminLayout,
       children: [
         {
-          path: 'home',
+          path: "/",
+          redirect: "/home/analysis"
+        },
+        {
+          path: '/home',
           name: 'home',
-          component: ()=>import('./views/home/Home.vue')
+          meta: {icon: 'dashboard', title: '主页'},
+          component: {render: h => h('router-view')},
+          children: [
+            {
+              path: 'analysis',
+              name: 'analysis',
+              meta: {title: '分析页'},
+              component: () => import('./views/home/Home.vue')
+            }
+          ]
+        },
+        {
+          path: '/table',
+          name: 'table',
+          meta: {icon: 'ios-list-box-outline', title: '表格'},
+          component: {render: h => h('router-view')},
+          children: [
+            {
+              path: 'basic_table',
+              name: 'basic_table',
+              meta: {title: '基础表格'},
+              component: () => import('./views/table/BasicTable.vue')
+            },
+            {
+              path: 'drag_table',
+              name: 'dynamic_table',
+              meta: {title: '可拖拽表格'},
+              component: () => import('./views/table/DragTable.vue')
+            }
+          ]
+        }, {
+          path: '/form',
+          name: 'form',
+          meta: {icon: 'ios-create-outline', title: '表单'},
+          component: {render: h => h('router-view')},
+          children: [
+            {
+              path: 'basic_form',
+              name: 'basic_form',
+              meta: {title: '基础表单'},
+              component: () => import('./views/form/BasicFrom.vue')
+            }
+          ]
         }
       ]
     },
     {
-      path: '/',
-      redirect: '/admin'
-    },
+      path: '/404',
+      hideInMenu: true,
+      name: 'NotFound',
+      conponent: () => import('./views/notfound/NotFound.vue')
+    }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== from.path) {
+    NProgress.start();
+  }
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
+export default router;
