d9d387463e85d1c8cebba90be16d4239fdadd745
walee <447563345@qq.com>
2019-06-15 10:26:54 +0800

    修复刷新页面导航激活和展开问题

--- src/layout/LeftMenu.vue
+++ src/layout/LeftMenu.vue
@@ -7,46 +7,42 @@
     :accordion="true"
     @on-select="handleSelect"
     @on-open-change="handleOpen"
+    ref="leftNav"
   >
     <Submenu name="home">
-            <template slot="title">
-                <Icon type="ios-analytics" />
-                主页
-            </template>
-            <MenuItem name="analysis" to='/home/analysis'>分析页</MenuItem>
-        </Submenu>
-        <Submenu name="table">
-            <template slot="title">
-                <Icon type="ios-filing" />
-                表格
-            </template>
-            <MenuItem name="basic_table"  to='/table/basic_table'>基础表格</MenuItem>
-            <MenuItem name="drag_table" to="/table/drag_table">可拖动表格</MenuItem>
-        </Submenu>
-        <Submenu name="form">
-            <template slot="title">
-                <Icon type="ios-cog" />
-                表单
-            </template>
-            <MenuItem name="basic_form" to='/form/basic_form'>基础表单</MenuItem>
-        </Submenu>
-        <Submenu name="chart">
-            <template slot="title">
-                <Icon type="ios-cog" />
-                图表
-            </template>
-            <MenuItem name="echart" to='/chart/echart'>echart</MenuItem>
-            <MenuItem name="highchart" to='/chart/highchart'>highchart</MenuItem>
-        </Submenu>
-        <Submenu name="map">
-            <template slot="title">
-                <Icon type="ios-cog" />
-                地图
-            </template>
-            <MenuItem name="baidu" to='/map/baidu'>百度地图</MenuItem>
-            <MenuItem name="google" to='/map/google'>谷歌地图</MenuItem>
-            <MenuItem name="gaode" to='/map/gaode'>高德地图</MenuItem>
-        </Submenu>
+      <template slot="title">
+        <Icon type="ios-analytics"/>主页
+      </template>
+      <MenuItem name="analysis" to="/home/analysis">分析页</MenuItem>
+    </Submenu>
+    <Submenu name="table">
+      <template slot="title">
+        <Icon type="ios-filing"/>表格
+      </template>
+      <MenuItem name="basic_table" to="/table/basic_table">基础表格</MenuItem>
+      <MenuItem name="drag_table" to="/table/drag_table">可拖动表格</MenuItem>
+    </Submenu>
+    <Submenu name="form">
+      <template slot="title">
+        <Icon type="ios-cog"/>表单
+      </template>
+      <MenuItem name="basic_form" to="/form/basic_form">基础表单</MenuItem>
+    </Submenu>
+    <Submenu name="chart">
+      <template slot="title">
+        <Icon type="ios-cog"/>图表
+      </template>
+      <MenuItem name="echart" to="/chart/echart">echart</MenuItem>
+      <MenuItem name="highchart" to="/chart/highchart">highchart</MenuItem>
+    </Submenu>
+    <Submenu name="map">
+      <template slot="title">
+        <Icon type="ios-cog"/>地图
+      </template>
+      <MenuItem name="baidu" to="/map/baidu">百度地图</MenuItem>
+      <MenuItem name="google" to="/map/google">谷歌地图</MenuItem>
+      <MenuItem name="gaode" to="/map/gaode">高德地图</MenuItem>
+    </Submenu>
   </Menu>
 </template>
 
@@ -61,16 +57,16 @@ export default {
   },
   data() {
     return {
-      openNames: ['home'],
+      openNames: [],
       activeName: this.$route.name
-    }
+    };
   },
   methods: {
-    handleSelect (v) {
-      this.activeName = v
+    handleSelect(v) {
+      this.activeName = v;
     },
-    handleOpen (v) {
-      this.openNames = v
+    handleOpen(v) {
+      this.openNames = v;
     }
   },
   components: {
@@ -78,8 +74,14 @@ export default {
     Submenu,
     MenuItem
   },
-  mounted () {
-
+  mounted() {
+    const names = this.$route.path.split("/");
+    this.activeName = names[names.length - 1];
+    this.openNames.push(names[1]);   // 这里要注意openNames的复制必须写在$$nextTick外面，不然不起作用，但activeName却可以写在里面
+    this.$nextTick(() => {
+      this.$refs.leftNav.updateOpened();
+      this.$refs.leftNav.updateActiveName();
+    });
   }
 };
 </script>
